b4d74503f0b401db4edf4279313680f66bc4c34a
Bogdan Ryzhko <m3meizumini@gmail.com>
2023-06-01 00:16:48 +0300

    add commander

--- contacts.js
+++ contacts.js
@@ -6,14 +6,12 @@ const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const listContacts = async () => {
 	const contacts = await fs.readFile(contactsPath, "utf-8");
-	// console.log(contacts);
-	return JSON.parse(contacts);;
+	return JSON.parse(contacts);
 };
 
 const getContactById = async (contactId) => {
 	const contacts = await listContacts();
 	const findContact = contacts.find(contact => contact.id === contactId);
-	console.log(findContact);
 	return findContact || null;
 }
 
@@ -31,12 +29,7 @@ const addContact = async (name, email, phone) => {
 	const repeatName = contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase());
 	if (repeatName) throw new Error(`${name} is in contacts`);
 
-	const newContact = {
-		id: uuidv4(),
-		name,
-		email,
-		phone
-	}
+	const newContact = { id: uuidv4(), name, email, phone };
 
 	contacts.push(newContact);
 	await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
--- index.js
+++ index.js
@@ -4,24 +4,37 @@ const {
 	removeContact,
 	addContact,
 } = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+
+program
+	.option("-a, --action, <type>")
+	.option("-i, --id, <type>")
+	.option("-n, --name, <type>")
+	.option("-e, --email, <type>")
+	.option("-p, --phone, <type>")
+
+program.parse();
+
+const argv = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
 	switch (action) {
 		case "list":
 			const contacts = await listContacts();
-			return console.log(contacts);
+			return console.table(contacts);
 		case "get":
 			const findedContact = await getContactById(id);
-			return console.log(findedContact);
+			return console.table(findedContact);
 		case "add":
 			const addedContact = await addContact(name, email, phone);
-			return console.log(addedContact);
+			return console.table(addedContact);
 		case "remove":
 			const removedContact = await removeContact(id);
-			return console.log(removedContact);
+			return console.table(removedContact);
 		default:
 			console.warn("\x1B[31m Unknown action type!");
 	}
 }
 
-invokeAction({ action: "remove", id: "f92d2351-463f-4c1d-bf44-8c9f7a7e9b8f" });
+invokeAction(argv);
